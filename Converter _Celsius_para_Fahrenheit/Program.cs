32f6fd8055c523c997ac70c99ecf4c99a0401c3b
Felipe Sales <felipebody-board@hotmail.com>
2025-03-14 15:56:13 +0000

    Adicionando .gitignore para ignorar pastas .vs/, bin/ e obj/

--- Converter _Celsius_para_Fahrenheit/Program.cs
+++ Converter _Celsius_para_Fahrenheit/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-// Programinha para converter temperatura de Fahrenheit para Celsius
+// Programinha para converter temperatura de Fahrenheit para  Graus Celsius
 
 namespace Converter__Celsius_para_Fahrenheit
 {
@@ -13,13 +13,20 @@ namespace Converter__Celsius_para_Fahrenheit
         static void Main(string[] args)
         {
             double temp = 0.0f;
+            double temp2 = 0.0f;
             Console.WriteLine("Indique a temperatura em Fahrenheit\n");
             temp = double.Parse(Console.ReadLine()); // converter para texto usando float.Parse
             Console.WriteLine();
+            Console.WriteLine("Indique a temperatura em graus Celsius\n");
+            temp2 = double.Parse(Console.ReadLine()); // converter para texto usando float.Parse
+            Console.WriteLine();
 
             double c = (temp - 32) / 1.8;
+            double f = (temp2 * 1.8) + 32;
 
-            Console.WriteLine($" A temperatura convertida em celsius é {c:f2} .");
+            Console.WriteLine($" A temperatura convertida em graus celsius é {c:f1} .");
+            Console.WriteLine();
+            Console.WriteLine($" A temperatura convertida em Fahrenheit é {f:f1} .");
         }
     }
 }
