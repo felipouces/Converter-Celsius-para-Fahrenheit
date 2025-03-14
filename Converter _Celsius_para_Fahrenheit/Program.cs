fcdaf850078c962c34e411a64ebf16af065747b4
Felipe Sales <felipebody-board@hotmail.com>
2025-03-14 15:43:54 +0000

    Primeiro commit: adicionando projeto inicial

---
+++ Converter _Celsius_para_Fahrenheit/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Programinha para converter temperatura de Fahrenheit para Celsius
+
+namespace Converter__Celsius_para_Fahrenheit
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double temp = 0.0f;
+            Console.WriteLine("Indique a temperatura em Fahrenheit\n");
+            temp = double.Parse(Console.ReadLine()); // converter para texto usando float.Parse
+            Console.WriteLine();
+
+            double c = (temp - 32) / 1.8;
+
+            Console.WriteLine($" A temperatura convertida em celsius é {c:f2} .");
+        }
+    }
+}
---
+++ Converter _Celsius_para_Fahrenheit/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// As informações gerais sobre um assembly são controladas por
+// conjunto de atributos. Altere estes valores de atributo para modificar as informações
+// associadas a um assembly.
+[assembly: AssemblyTitle("Converter _Celsius_para_Fahrenheit")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Converter _Celsius_para_Fahrenheit")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Definir ComVisible como false torna os tipos neste assembly invisíveis
+// para componentes COM. Caso precise acessar um tipo neste assembly de
+// COM, defina o atributo ComVisible como true nesse tipo.
+[assembly: ComVisible(false)]
+
+// O GUID a seguir será destinado à ID de typelib se este projeto for exposto para COM
+[assembly: Guid("716f3a4f-772d-4652-ba32-e72379607636")]
+
+// As informações da versão de um assembly consistem nos quatro valores a seguir:
+//
+//      Versão Principal
+//      Versão Secundária 
+//      Número da Versão
+//      Revisão
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
